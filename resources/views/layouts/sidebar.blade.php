234a7d8565938698a1c093a0f9711006df9a8a13
Marco Estrada Lopez <fericell2909@gmail.com>
2019-10-11 10:16:35 -0500

    Tabla de : dre , ugel , locales

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -91,7 +91,6 @@ function current_page($url = '/'){
           </a>
           <ul class="treeview-menu">
             <li><a href="{{route("registrofichas")}}"><i class="fa fa-street-view" aria-hidden="true"></i> Registro de Fichas</a></li>
-            <li><a href="{{route("ubicaciones")}}"><i class="fa fa-street-view" aria-hidden="true"></i> Registro de Fichas 2</a></li>
             <li><a href="{{route("UbicacionesListar")}}"><i class="fa fa-list" aria-hidden="true"></i> Listado de Fichas</a></li>
           </ul>
         </li>
@@ -144,7 +143,7 @@ function current_page($url = '/'){
               </span>
           </a>
           <ul class="treeview-menu">
-            <li><a href="{{route("ubicaciones")}}"><i class="fa fa-street-view" aria-hidden="true"></i> Registro de Fichas</a></li>
+            <li><a href="{{route("registrofichas")}}"><i class="fa fa-street-view" aria-hidden="true"></i> Registro de Fichas</a></li>
           </ul>
         </li>
       @endif
